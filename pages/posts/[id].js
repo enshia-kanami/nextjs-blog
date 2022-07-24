93342ee5e45c7b30dfa0699324715effa80e8d89
higashimoto-kanami <>
2022-07-24 09:28:02 +0900

    とらゼミ#6-2

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,28 +1,37 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
 
 export default function Post({ postData }) {
     return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
+        <Layout>
+          <Head>
+            <title>{postData.title}</title>
+          </Head>
+            <article>
+              <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
     );
-}
+  }
 
-export async function getStaticPaths() { //準備されている関数
+export async function getStaticPaths() { 
     const paths = getAllPostIds();
     return {
-        paths,  //配列で中はオブジェクト 事前ビルドして
-        fallback: false, //指定パス以外なら404を返す
+        paths,
+        fallback: false
     };
 }
 
-export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+  export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
     return {
       props: {
         postData,
